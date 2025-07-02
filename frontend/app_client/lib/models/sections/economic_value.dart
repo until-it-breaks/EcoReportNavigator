faa1b3f62289310edcb5d337667aeb22e604fa60
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-07-02 18:22:22 +0200

    Defined model for economic value

---
+++ frontend/app_client/lib/models/sections/economic_value.dart
@@ -0,0 +1,93 @@
+class EconomicValueSummary {
+  final ValueWithUnit attractedValue;
+  final ValueWithUnit distributedValue;
+  final ValueWithUnit fivePerThousand;
+  final String greenPurchasesPercentage;
+
+  EconomicValueSummary({
+    required this.attractedValue,
+    required this.distributedValue,
+    required this.fivePerThousand,
+    required this.greenPurchasesPercentage,
+  });
+
+  factory EconomicValueSummary.fromJson(Map<String, dynamic> json) {
+    return EconomicValueSummary(
+      attractedValue: ValueWithUnit.fromJson(json["valore_attratto"]),
+      distributedValue: ValueWithUnit.fromJson(json["valore_distribuito"]),
+      fivePerThousand: ValueWithUnit.fromJson(json["5x1000"]),
+      greenPurchasesPercentage: json["percentuale_acquisti_verdi"],
+    );
+  }
+}
+
+class ValueWithUnit {
+  final num value;
+  final String unitOfMeasure;
+
+  ValueWithUnit({required this.value, required this.unitOfMeasure});
+
+  factory ValueWithUnit.fromJson(Map<String, dynamic> json) {
+    return ValueWithUnit(
+      value: json["valore"],
+      unitOfMeasure: json["unita_di_misura"],
+    );
+  }
+}
+
+class EconomicValueDetailed {
+  final String unitOfMeasure;
+  final List<EconomicEntry> entries;
+
+  EconomicValueDetailed({required this.unitOfMeasure, required this.entries});
+
+  factory EconomicValueDetailed.fromJson(Map<String, dynamic> json) {
+    final list = json["voci"] as List<dynamic>;
+    final entriesList = list.map((e) => EconomicEntry.fromJson(e)).toList();
+
+    return EconomicValueDetailed(
+      unitOfMeasure: json["unita_di_misura"],
+      entries: entriesList,
+    );
+  }
+}
+
+class EconomicEntry {
+  final String name;
+  final num value;
+  final String percentage;
+
+  EconomicEntry({
+    required this.name,
+    required this.value,
+    required this.percentage,
+  });
+
+  factory EconomicEntry.fromJson(Map<String, dynamic> json) {
+    return EconomicEntry(
+      name: json["nome"],
+      value: json["valore"],
+      percentage: json["percentuale"],
+    );
+  }
+}
+
+class GreenPurchases {
+  final String percentageOf2022;
+  final String percentageOf2023;
+  final String targetPercentageOf2024;
+
+  GreenPurchases({
+    required this.percentageOf2023,
+    required this.percentageOf2022,
+    required this.targetPercentageOf2024,
+  });
+
+  factory GreenPurchases.fromJson(Map<String, dynamic> json) {
+    return GreenPurchases(
+      percentageOf2022: json["percentuale_2022"],
+      percentageOf2023: json["percentuale_2023"],
+      targetPercentageOf2024: json["target_2024"],
+    );
+  }
+}

8cdacf13e0ddf8ccfbbced0ef4a78d4df0b86c5f
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-07-03 16:18:45 +0200

    Replace quotes with double quotes

--- frontend/app_client/lib/models/sections/environment.dart
+++ frontend/app_client/lib/models/sections/environment.dart
@@ -97,9 +97,9 @@ class CarbonFootprintEntry {
 
   factory CarbonFootprintEntry.fromJson(Map<String, dynamic> json) {
     return CarbonFootprintEntry(
-      area: json['area'],
-      year2022: json['2022'],
-      year2023: json['2023'],
+      area: json["area"],
+      year2022: json["2022"],
+      year2023: json["2023"],
     );
   }
 }
@@ -112,10 +112,10 @@ class CarbonFootprint {
 
   factory CarbonFootprint.fromJson(Map<String, dynamic> json) {
     var list =
-        (json['dati'] as List)
+        (json["dati"] as List)
             .map((item) => CarbonFootprintEntry.fromJson(item))
             .toList();
 
-    return CarbonFootprint(unitOfMeasure: json['unita_di_misura'], data: list);
+    return CarbonFootprint(unitOfMeasure: json["unita_di_misura"], data: list);
   }
 }

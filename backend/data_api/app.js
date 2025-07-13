1d9dd58affeaa522d7ac2754d3d57630b38aea0a
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-07-13 21:05:23 +0200

    Moved backend projects in their right directory

--- backend/data_api/app.js
+++ backend/data_api/app.js
@@ -1,29 +1,20 @@
 import express from "express"
 import cors from "cors"
-import dotenv from "dotenv"
 
-import GeminiClient from "./src/llm/gemini-client.js"
-import createLLMRoute from "./src/routes/llm.routes.js"
 import uiDataRoute from "./src/routes/ui-data.routes.js";
 
 import logger from "./src/utils/logger.js";
 
 const TAG = "[startup]";
 
-dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const llm = new GeminiClient(process.env.GEMINI_API_KEY);
-
-app.use("/ask", createLLMRoute(llm));
-logger.info(`${TAG} Mounted /ask route`);
 app.use("/ui-data", uiDataRoute);
 logger.info(`${TAG} Mounted /ui-data route`);
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 const server = app.listen(PORT, "0.0.0.0", () => {
     const { address, port } = server.address();
--- backend/server/src/llm/llm-interface.js
+++
@@ -1,6 +0,0 @@
-export class LLMInterface {
-    // eslint-disable-next-line no-unused-vars
-    async ask(_prompt) {
-        throw new Error("Method 'ask' not implemented")
-    }
-}
